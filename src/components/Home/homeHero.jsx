09b270f7d775d0ad18710c1dc792e15c57b61803
hiten Dabhi <you@example.com>
2021-08-02 16:03:13 +0100

    banner changed

--- src/components/Home/homeHero.jsx
+++ src/components/Home/homeHero.jsx
@@ -56,38 +56,6 @@ class HomeBanner extends Component {
                 style={{ backgroundImage: `url(${Brumbanner})` }}
               ></div>
             </div>
-            <div>
-              <div
-                className='carosel-image'
-                style={{ backgroundImage: `url(${BannerImageTwo})` }}
-              ></div>
-            </div>
-
-            <div>
-              <div
-                className='carosel-image'
-                style={{ backgroundImage: `url(${BannerImageThree})` }}
-              >
-                <button
-                  onClick={() => this.openbanner("https://ultimomerch.com/")}
-                  className='hero-button'
-                ></button>
-              </div>
-            </div>
-            <div>
-              <div
-                className='carosel-image fix-height'
-                style={{
-                  backgroundImage: `url(${BannerImageFour})`,
-                  backgroundPosition: "0 -150px",
-                }}
-              >
-                <button
-                  onClick={() => this.openbanner("https://ultimomerch.com/")}
-                  className='hero-button'
-                ></button>
-              </div>
-            </div>
           </Slider>
         </div>
         <div
