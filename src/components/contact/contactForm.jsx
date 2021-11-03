18e9b2b7a37485d5c80adc904a8eb1e41a6f7916
Simon Brown <uptoeleven@gmail.com>
2021-11-03 22:36:35 +0000

    remove old forms, switch to emailOctopus

--- src/components/contact/contactForm.jsx
+++ src/components/contact/contactForm.jsx
@@ -7,7 +7,7 @@ class ContactForm extends Component {
   state = {};
   render() {
     return (
-        <script async src='https://eomail5.com/form/1a71a4a0-37f0-11ec-96e5-06b4694bee2a.js' data-form='1a71a4a0-37f0-11ec-96e5-06b4694bee2a'></script>
+        <script async src="https://eomail5.com/form/1a71a4a0-37f0-11ec-96e5-06b4694bee2a.js" data-form="1a71a4a0-37f0-11ec-96e5-06b4694bee2a"></script>
     );
   }
 }
